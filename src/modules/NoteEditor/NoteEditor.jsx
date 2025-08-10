cbee50539a4f8c258ae34e545df527f785169fae
DuskStarGITHUB <contactspeencer@gmail.com>
2025-08-09 21:31:50 -0600

    UI UPDATE 2.1

--- src/modules/NoteEditor/NoteEditor.jsx
+++ src/modules/NoteEditor/NoteEditor.jsx
@@ -19,15 +19,15 @@ export default function NoteEditor({
   return (
     <main className="flex-1 flex flex-col p-8 bg-transparent space-y-4 overflow-hidden">
       <NoteTitleInput title={note.name} onRename={onRenameNote} note={note} />
-      <div className="flex flex-1 gap-4">
-        <div className="flex-1 flex flex-col">
+      <div className="flex flex-1 gap-4 overflow-hidden">
+        <div className="flex-1 flex flex-col h-full overflow-y-auto">
           <Editor
             value={content}
             onChange={onChangeContent}
-            className="flex-1 bg-zinc-900 rounded-md overflow-hidden"
+            className="flex-1 bg-zinc-900 rounded-md"
           />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 h-full overflow-y-auto">
           <MarkdownPreview content={content} />
         </div>
       </div>
